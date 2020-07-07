ab4896d8b5e1c308c898b7ccb1d372cf8b13009f
Fabian Schindler <fabian.schindler.strauss@gmail.com>
2020-07-07 17:05:36 +0200

    Imrpoving DAPA interfaces

--- edc_ogc/dapa.py
+++ edc_ogc/dapa.py
@@ -1,10 +1,36 @@
 from textwrap import dedent
 import _ast
+from itertools import product
+import os
 
 from dateutil.parser import parse
-from flask import Blueprint, request
+from flask import Blueprint, request, Response
 from eoxserver.core.util.timetools import parse_iso8601, parse_duration
 from eoxserver.render.browse.generate import parse_expression, extract_fields
+
+from edc_ogc.configapi import ConfigAPIDefaultLayers
+
+
+CONFIG_CLIENT = None
+
+def get_config_client():
+    global CONFIG_CLIENT
+    datasets_path = os.environ.get('DATASETS_PATH')
+    layers_path = os.environ.get('LAYERS_PATH')
+    dataproducts_path = os.environ.get('DATAPRODUCTS_PATH')
+    client_id = os.environ.get('SH_CLIENT_ID')
+    client_secret = os.environ.get('SH_CLIENT_SECRET')
+
+    if CONFIG_CLIENT is None:
+        CONFIG_CLIENT = ConfigAPIDefaultLayers(
+            client_id,
+            client_secret,
+            datasets_path=datasets_path,
+            layers_path=layers_path,
+            dataproducts_path=dataproducts_path
+        )
+
+    return CONFIG_CLIENT
 
 
 dapa = Blueprint('dapa', __name__)
@@ -22,7 +48,6 @@ dapa = Blueprint('dapa', __name__)
         area/
         position/
 '''
-
 
 def parse_fields(value):
     fields = value.split(',')
@@ -44,6 +69,13 @@ def parse_fields(value):
 
 def parse_aggregates(value):
     return value.split(',')
+
+
+def parse_bbox(value):
+    bbox = [float(v) for v in value.split(',')]
+    if len(bbox) not in (4, 6):
+        raise ValueError('Invalid number of elements in bbox')
+    return bbox
 
 
 def parse_time(value):
@@ -89,6 +121,11 @@ def expressions_to_evalscript(fields, inputs, aggregates):
         else:
             dynamic_fields.append((name, eval_expression(value)))
 
+    out_fields = [
+        f'agg_{agg_method}(samples, "{name}")'
+        for name, _ in fields for agg_method in aggregates
+    ]
+
     return dedent(f"""\
         //VERSION=3
         function setup() {{
@@ -102,33 +139,40 @@ def expressions_to_evalscript(fields, inputs, aggregates):
             }};
         }}
 
-        function agg_min(samples, field, method) {{
+        function agg_min(samples, field) {{
             return samples.reduce((acc, sample) => Math.min(acc, sample[field]));
         }}
 
-        function agg_max(samples, field, method) {{
+        function agg_max(samples, field) {{
             return samples.reduce((acc, sample) => Math.max(acc, sample[field]));
         }}
 
-        function agg_stdev(samples, field, method) {{
+        function agg_avg(samples, field) {{
+            return samples.reduce((acc, sample) => acc + sample[field]) / samples.length;
+        }}
+
+        function agg_stdev(samples, field) {{
+            const mean = agg_avg(samples, field);
             return Math.sqrt(
-                samples.reduce(
-                    (acc, sample) => acc + Math.pow(sample[field])
-                ), 0
-            ) / (samples.length - 1));
+                samples.reduce((acc, sample) => acc + Math.pow(sample[field] - mean, 2), 0) / (samples.length - 1)
+            );
         }}
 
         function evaluatePixel(samples, scenes, inputMetadata, customData, outputMetadata) {{
-            samples = samples.map(sample => {{
+            samples = samples.map(sample => ({{
                 {' '.join(f'{field}: sample.{field},' for field in static_fields)}
                 {' '.join(f'{name}: {expr},' for name, expr in dynamic_fields)}
-            }});
+            }}));
 
             return [
+                {', '.join(out_fields)}
             ];
         }}
     """)
 
+#
+#  -------------- Routes
+#
 
 @dapa.route('/<collection>/dapa/fields')
 def fields(collection_id):
@@ -145,8 +189,30 @@ def area(collection):
     fields, inputs = parse_fields(request.args['fields'])
     aggregates = parse_aggregates(request.args['aggregate'])
     evalscript = expressions_to_evalscript(fields, inputs, aggregates)
+    time = parse_time(request.args['time'])
+
+
     print(evalscript)
-    return evalscript
+
+    if 'bbox' in request.args:
+        bbox = parse_bbox(request.args['bbox'])
+    else:
+        raise NotImplementedError('Currently bbox is required')
+
+    client = get_config_client()
+
+    response = client.get_mdi().process_image(
+        [{'type': collection}],
+        bbox,
+        crs='http://www.opengis.net/def/crs/EPSG/0/4326',
+        width=512,
+        height=512,
+        format='image/tiff',
+        evalscript=evalscript,
+        time=time
+    )
+
+    return Response(response, mimetype='image/tiff')
 
 
 @dapa.route('/<collection>/dapa/timeseries/area')
